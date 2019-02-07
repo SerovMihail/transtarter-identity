c97eb5436adcfe477a329313b1dbe3ba2af751f9
SerovMikhail <serov.mikhail.job@gmail.com>
2019-02-07 16:19:20 +0200

    add degu hosts

--- ts_identityserver/Skoruba.IdentityServer4.Admin/Constants/AuthorizationConsts.cs
+++ ts_identityserver/Skoruba.IdentityServer4.Admin/Constants/AuthorizationConsts.cs
@@ -11,9 +11,24 @@ namespace Skoruba.IdentityServer4.Admin.Constants
         public const string AdministrationRole = "SkorubaIdentityAdminAdministrator";
 
         public const string IdentityAdminCookieName = "IdentityServerAdmin";
+
+#if DEBUG
+        public const string IdentityAdminRedirectUri = "http://localhost:9000/signin-oidc";
+#else
         public const string IdentityAdminRedirectUri = "http://192.168.39.103:9000/signin-oidc";
+#endif
+
+#if DEBUG
+        public const string IdentityServerBaseUrl = "http://localhost:5000/";
+#else
         public const string IdentityServerBaseUrl = "http://192.168.39.103:5000";
+#endif
+
+#if DEBUG
+        public const string IdentityAdminBaseUrl = "http://localhost:9000/";
+#else
         public const string IdentityAdminBaseUrl = "http://192.168.39.103:9000";
+#endif
 
         public const string UserNameClaimType = "name";
         public const string SignInScheme = "Cookies";
